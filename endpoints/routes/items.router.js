ed6b9340f6017588c97114fef401c999055e2758
gabi04 <gsuarezc04@unal.edu.co>
2023-05-19 10:18:47 -0500

    chore: server, items?q pagination

--- endpoints/routes/items.router.js
+++ endpoints/routes/items.router.js
@@ -1,16 +1,39 @@
 const express = require("express");
 
-const {
-  getItemsByQuery,
-  getItemDetail,
-} = require("./../services/items.service");
+const { getItems, getItemDetail } = require("./../services/items.service");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const { q: query } = req.query;
-  const items = await getItemsByQuery(query);
-  res.json(items);
+  const query = req.query.q;
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const items = await getItems(query);
+  let itemsResponse = {
+    ...items,
+    items: items.items.slice(startIndex, endIndex),
+  };
+  if (startIndex > 0) {
+    itemsResponse = {
+      previus: {
+        page: page - 1,
+      },
+      ...itemsResponse,
+    };
+  }
+  if (endIndex < items.length) {
+    itemsResponse = {
+      next: {
+        page: page + 1,
+      },
+      ...itemsResponse,
+    };
+  }
+
+  res.json(itemsResponse);
 });
 
 router.get("/:id", async (req, res) => {
