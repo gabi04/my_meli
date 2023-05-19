7570833972e21f73bf17d395cf068b9deb9399f8
gabi04 <gsuarezc04@unal.edu.co>
2023-05-19 06:27:19 -0500

    chore: remove console log

--- my_meli_front/src/components/Loader/index.js
+++ my_meli_front/src/components/Loader/index.js
@@ -2,7 +2,6 @@ import "./Loader.sass";
 
 const Loader = () => {
   const iterable = Array.from(Array(12).keys());
-  console.log(iterable);
   return (
     <div className="lds-spinner__container">
       <div className="lds-spinner">
